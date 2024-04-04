beefce2185dce09f04885ac331cee03205659ccf
ThuatVDQ <thuatvopro@gmail.com>
2024-04-04 22:13:02 +0700

    fix: get products with keyword and category id

--- src/main/java/com/project/shopapp/controllers/ProductController.java
+++ src/main/java/com/project/shopapp/controllers/ProductController.java
@@ -124,14 +124,16 @@ public class ProductController {
 
     @GetMapping("")
     public ResponseEntity<List<ProductListResponse>> getProducts(
-            @RequestParam("page") int page,
-            @RequestParam("limit") int limit
+            @RequestParam(defaultValue = "") String keyword,
+            @RequestParam(defaultValue = "", name = "category_id") Long categoryId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int limit
     ) {
         PageRequest pageRequest = PageRequest.of(
                 page, limit,
-                Sort.by("createdAt").descending()
+                Sort.by("id").ascending()
         );
-        Page<ProductResponse> productPage = productService.getAllProducts(pageRequest);
+        Page<ProductResponse> productPage = productService.getAllProducts(keyword, categoryId, pageRequest);
 
         int totalPages = productPage.getTotalPages();
         List<ProductResponse> products = productPage.getContent();
